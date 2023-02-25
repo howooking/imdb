0117c10e5df7ffc938cfe2b6c67889d4c24201b4
howooking <junsgk@gmail.com>
2023-02-26 01:54:11 +0900

    darkmode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,18 @@
-import { BsFillSunFill } from "react-icons/bs";
 import { AiFillHome } from "react-icons/ai";
 import { AiFillInfoCircle } from "react-icons/ai";
 import MenuItem from "./MenuItem";
+import DarkmodeSwitch from "./DarkmodeSwitch";
 
 export default function Header() {
   return (
-    <header className='bg-gray-700 py-7'>
-      <div className='mx-auto flex max-w-3xl items-center justify-between font-bold text-white'>
+    <header className='py-7'>
+      <div className='mx-auto flex max-w-3xl items-center justify-between font-bold'>
         <div className='flex gap-6'>
           <MenuItem href='/' Icon={AiFillHome} title='Home' />
           <MenuItem href='/about' Icon={AiFillInfoCircle} title='ABOUT' />
         </div>
         <div className='flex items-center gap-7'>
-          <BsFillSunFill className='text-2xl' />
+          <DarkmodeSwitch />
           <div className='rounded-lg bg-orange-400 p-2'>IMDB</div>
           <div>Clone</div>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className='bg-gray-400 py-4'>
-      <div className='flex justify-center gap-8 font-bold text-white'>
+    <nav className='py-4'>
+      <div className='flex justify-center gap-8 font-bold'>
         <Link href='/treding'>Trending</Link>
         <Link href='/toprated'>Top Rated</Link>
       </div>
